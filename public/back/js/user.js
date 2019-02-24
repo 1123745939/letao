bbfd7e49b1aae352b55487d6eeb8f3bf0c4db143
ybq <1123745939@qq.com>
2019-02-24 15:17:23 +0800

    用户管理基本完成

---
+++ public/back/js/user.js
@@ -0,0 +1,16 @@
+$(function () {
+    $.ajax({
+        tyep: "get",
+        url: "/user/queryUser",
+        data: {
+            page: 1,
+            pageSize: 5
+        },
+        dataType: "json",
+        success: function (info) {
+            console.log(info);
+            var htmlStr = template( 'tpl', info )
+            $('tbody').html( htmlStr )
+        }
+    })    
+})
